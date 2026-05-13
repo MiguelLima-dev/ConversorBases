efb609e2fad336176e337bbc5a4107ff63607137
Miguel Augusto de LIma <miguelaugustolima07@gmail.com>
2026-05-13 10:11:09 -0300

    Add files via upload
    Primeiro Commit

---
+++ conversao.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Converte decimal para octal
+	int decimal;
+	cin >> decimal;
+	
+	string octal = "";
+    int temp = decimal;
+	while (decimal != 0){
+		octal = char((decimal % 8) + '0') + octal;
+		decimal /= 8;
+	}
+	
+	cout << "Octal: " << octal << endl;
+
+    // Converte decimal para binário
+    decimal = temp;
+	string binario = "";
+	while (decimal != 0){
+		binario = char((decimal % 2) + '0') + binario;
+		decimal /= 2;
+	}
+	
+	cout << "Decimal: " << binario << endl;
+
+    // Converte decimal para hexadecimal
+    decimal = temp;
+    string hexadecimal = "";
+    string digitos = "0123456789ABCDEF";
+
+    while (decimal != 0) {
+        hexadecimal = digitos[decimal % 16] + hexadecimal;
+        decimal /= 16;
+    }
+
+    cout << "Hexadecimal: " << hexadecimal << endl;
+	return 0;
+}
