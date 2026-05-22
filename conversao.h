af0c874984e5116b490b54b52aad681e1526a77d
Miguel Lima <miguelaugustolima07@gmail.com>
2026-05-22 15:05:59 -0300

    Converte fracionários para Binário.

--- conversao.h
+++ conversao.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 // DECIMAL P/ BINÁRIO.
@@ -13,6 +14,30 @@ string deci_bin(int decimal) {
 	}
 
 	return binario;
+}
+
+// TRANFORMA UM NÚMERO EM UMA STRING EM UM DOUBLE.
+double stringDouble(string s) {
+	double resultado = 0.0;	
+	double divisor = 10.0;
+	bool separador = false;
+
+	for (int i = 0; s[i] != '\0'; i++) {
+		char c = s[i];
+		if (c == ',' || c == '.')
+			separador = true;
+		else {
+			int digito = c - '0';
+			if (!separador) {
+				resultado = resultado * 10.0 + digito;
+			} else {
+				resultado += digito / divisor;
+				divisor *= 10.0;
+			}
+		}
+	}
+
+	return resultado;
 }
 
 // DECIMAL FRACIONARIO P/ BINÁRIO.
