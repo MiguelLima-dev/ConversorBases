a7cae6d74581274879d132a20c470e62df5bdedc
CarolF16 <anacarolinavsilvasantos009@gamil.com>
2026-05-24 14:37:48 -0300

    Adição das funções do F3 e F4 concluída

--- principal.cpp
+++ principal.cpp
@@ -6,12 +6,18 @@ using namespace std;
 
 int main(){
     cout << "=-=-=-=-= MENU =-=-=-=-=" << endl;
-    cout << "1. Converter Decimal para Binário" << endl;
-    cout << "2. Converter Decimal para Octal" << endl;
-    cout << "3. Converter Decimal para Hexadecimal" << endl;
-    cout << "4. Converte Binário para Decimal." << endl;
-    cout << "5. Converte Octal para Decimal." << endl;
-    cout << "6. Converte Hexadecimal para Decimal." << endl;
+    cout << "1. Converter Decimal para Binário." << endl;
+    cout << "2. Converter Decimal para Octal." << endl;
+    cout << "3. Converter Decimal para Hexadecimal." << endl;
+    cout << "4. Converter Binário para Decimal." << endl;
+    cout << "5. Converter Octal para Decimal." << endl;
+    cout << "6. Converter Hexadecimal para Decimal." << endl;
+    cout << "7. Converter Binário para Octal." << endl;
+    cout << "8. Converter Binário para Hexadecimal." << endl;
+    cout << "9. Converter Octal para Binário." << endl;
+    cout << "10. Converter Octal para Hexadecimal." << endl;
+    cout << "11. Converter Hexadecimal para Binário." << endl;
+    cout << "12. Converter Hexadecimal para Octal." << endl;
 
     int modo;
     cin >> modo;
@@ -83,10 +89,58 @@ int main(){
                 if (fracionaria != "") 
                     resultado += hexaFrac_deci(fracionaria);
                 
-                imprimir(doubleString(resultado), 8, 10);
+                imprimir(doubleString(resultado), 16, 10);
             }
             break;
 
+		case 7:
+			cout << "Binário: ";
+			cin >> entrada;
+			if (valida(entrada, 2)) {
+				imprimir(bin_oct(entrada), 2, 8);
+			}
+			break;
+			
+		case 8:
+			cout << "Binário: ";
+			cin >> entrada;
+			if (valida(entrada, 2)) {
+				imprimir(bin_hexa(entrada), 2, 16);
+			}
+			break;
+			
+		case 9:
+			cout << "Octal: ";
+			cin >> entrada;
+			if (valida(entrada,8)) {
+				imprimir(oct_bin(entrada), 8, 2);
+			}
+			break;
+			
+		case 10:
+			cout << "Octal: ";
+			cin >> entrada;
+			if (valida(entrada, 8)) {
+				imprimir(oct_hexa(entrada), 8, 16);
+			}
+			break;
+			
+		case 11:
+			cout << "Hexadecimal: ";
+			cin >> entrada;
+			if (valida(entrada, 16)) {
+				imprimir(hexa_bin(entrada), 16, 2);
+			}
+			break;
+			
+		case 12:
+			cout << "Hexadecimal: ";
+			cin >> entrada;
+			if (valida(entrada, 16)) {
+				imprimir(hexa_oct(entrada), 16, 8);
+			}
+			break;
+			
         default:
             cout << "Opção inexistente" << endl;
     }
